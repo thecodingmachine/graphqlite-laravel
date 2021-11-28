5242f392d95399c644f74f1ffe3e7f6f3ef2a66d
Minh Vuong <38932626+vuongxuongminh@users.noreply.github.com>
2021-11-28 18:04:37 +0700

    fix: `debug` setting in graphqlite config file
    `GraphQL\Error\Debug` was renamed to `GraphQL\Error\DebugFlag` in WebonyxGraphQL version 14.

--- config/graphqlite.php
+++ config/graphqlite.php
@@ -1,6 +1,6 @@
 <?php
 
-use GraphQL\Error\Debug;
+use GraphQL\Error\DebugFlag;
 
 return [
     /*
@@ -18,7 +18,7 @@ return [
      */
     'controllers' => 'App\\Http\\Controllers',
     'types' => 'App\\',
-    'debug' => Debug::RETHROW_UNSAFE_EXCEPTIONS,
+    'debug' => DebugFlag::RETHROW_UNSAFE_EXCEPTIONS,
     'uri' => env('GRAPHQLITE_URI', '/graphql'),
     'middleware' =>  ['web'],
 ];
