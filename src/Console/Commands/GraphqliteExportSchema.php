3acd07355a3d14c2f698eebeaf176f7d7ff52779
David Négrier <d.negrier@thecodingmachine.com>
2023-04-28 19:22:04 +0200

    Addig a graphqlite:export-schema command
    This new Laravel command can be run using
    
    ```
    ./artisan graphqlite:export-schema --output file.schema
    ```
    
    It will output the content of the Schema to an SDL file.
    This can be useful for instance for tools like graphql-codegen.

---
+++ src/Console/Commands/GraphqliteExportSchema.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace TheCodingMachine\GraphQLite\Laravel\Console\Commands;
+
+use GraphQL\Utils\SchemaPrinter;
+use Illuminate\Console\Command;
+use TheCodingMachine\GraphQLite\Schema;
+
+/**
+ * A command to export the GraphQL schema in "Schema Definition Language" (SDL) format.
+ */
+class GraphqliteExportSchema extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'graphqlite:export-schema {--O|output= : Output file name. If not specified, prints on stdout}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Exports the GraphQL schema in "Schema Definition Language" (SDL) format.';
+
+    public function __construct(private Schema $schema)
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $output = $this->option('output');
+
+        $sdl = SchemaPrinter::doPrint($this->schema, [
+            "sortArguments" => true,
+            "sortEnumValues" => true,
+            "sortFields" => true,
+            "sortInputFields" => true,
+            "sortTypes" => true,
+        ]);
+
+        if ($output === null) {
+            $this->line($sdl);
+        } else {
+            file_put_contents($output, $sdl);
+        }
+
+        return Command::SUCCESS;
+    }
+}
